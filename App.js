b9155ef630456481ceec7560a3b20f5083d28706
lao_j <lao_j@etna-alternance.net>
2022-06-30 15:48:32 +0200

    redux

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import {createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import username from './reducers/username'
 
 import { Ionicons } from '@expo/vector-icons';
 
@@ -11,11 +14,12 @@ import MapScreen from './screens/MapScreen';
 import ScanScreen from './screens/ScanScreen';
 import CardScreen from './screens/CardScreen';
 import ProfilScreen from './screens/ProfilScreen';
+import LoginScreen from './screens/LoginScreen';
 
-
-
+const store = createStore(combineReducers({username}))
  const Stack = createStackNavigator();
  const Tab = createBottomTabNavigator();
+
 
  const BottomNavigator = () => {
     return (
@@ -24,13 +28,13 @@ import ProfilScreen from './screens/ProfilScreen';
           tabBarIcon: ({ color }) => {
             let iconName;
   
-            if (route.name === 'Home') {
+            if (route.name === 'Accueil') {
               iconName = 'md-home';
             } else if (route.name === 'Map') {
               iconName = 'map-outline';
             } else if (route.name === 'Scan') {
               iconName = 'qr-code-outline';
-            } else if (route.name === 'Card') {
+            } else if (route.name === 'Cartes') {
               iconName = 'card-outline';
             } else if (route.name === 'Profil') {
               iconName = 'person-circle-outline';
@@ -40,26 +44,25 @@ import ProfilScreen from './screens/ProfilScreen';
           },
         })}
         tabBarOptions={{
-          // showLabel:false,
+          showLabel:false,
           style:{
-            position: 'absolute',
-            bottom: 15,
-            left:15,
-            right:15,
-            elevation:0,
-            borderRadius:15,
-            height:90,
+            // position: 'absolute',
+          //   bottom: 15,
+          //   left:15,
+          //   right:15,
+          //   elevation:0,
+            // borderRadius:15,
+            height:70,
+            paddingBottom:10,
           },
           activeTintColor: '#5DB075',
        inactiveTintColor: '#dfe6e9',
         }}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
-
+        <Tab.Screen name="Accueil" component={HomeScreen} />
         <Tab.Screen name="Map" component={MapScreen} />
         <Tab.Screen name="Scan" component={ScanScreen} />
-
-        <Tab.Screen name="Card" component={CardScreen} />
+        <Tab.Screen name="Cartes" component={CardScreen} />
         <Tab.Screen name="Profil" component={ProfilScreen} />
       </Tab.Navigator>
     )
@@ -67,11 +70,13 @@ import ProfilScreen from './screens/ProfilScreen';
  
  export default function App() {
   return (
+    <Provider store={store}>
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}> 
-        {/* <Stack.Screen name='Home' component={HomeScreen} /> */}
+        <Stack.Screen name='Login' component={LoginScreen} />
         <Stack.Screen name='BottomNavigator' component={BottomNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
+    </Provider>
   );
   }
