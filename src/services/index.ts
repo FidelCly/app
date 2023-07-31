297df35337d0c8461eb690407f03b90b01b382fa
Andi MANIETA <andimanieta@gmail.com>
2023-07-31 14:57:55 +0200

    fix: map

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,5 @@ export { login, register } from "./auth-service";
 export { getCardBalanceById } from "./balance-service";
 export { addCardToWallet, deleteCard, getUserCards, updateCard } from "./card-service";
 export { getShopPromotions } from "./promotion-service";
-export { getNearShops, getShopById } from "./shop-service";
+export { getAllShops, getNearShops, getShopById } from "./shop-service";
 export { getUserById } from "./user-service";
--- src/services/shop-service.ts
+++ src/services/shop-service.ts
@@ -1,16 +1,52 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 /**
  * getShopById
  * @param shopId
  * @returns
  */
 export const getShopById = async (shopId: number) => {
-  const url = process.env.API_URL + "/shop/" + shopId;
-  try {
-    const response = await fetch(url);
-    return response.json();
-  } catch (error) {
-    return error;
-  }
+	const url = process.env.API_URL + "/shop/" + shopId;
+	try {
+		const token = await AsyncStorage.getItem("token");
+
+		const response = await fetch(url, {
+			method: "GET",
+			mode: "no-cors",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+				Authorization: `Bearer ${token}`
+			}
+		});
+		return response.json();
+	} catch (error) {
+		return error;
+	}
+};
+
+/**
+ * getAllShops
+ * @returns
+ */
+export const getAllShops = async () => {
+	const url = process.env.API_URL + "/shop/";
+	try {
+		const token = await AsyncStorage.getItem("token");
+
+		const response = await fetch(url, {
+			method: "GET",
+			mode: "no-cors",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+				Authorization: `Bearer ${token}`
+			}
+		});
+		return response.json();
+	} catch (error) {
+		return error;
+	}
 };
 
 /**
@@ -20,17 +56,29 @@ export const getShopById = async (shopId: number) => {
  * @param lng
  * @returns
  */
-export const getNearShops = async (
-  distance: number,
-  lat: number,
-  lng: number
-) => {
-  const url =
-    process.env.API_URL + `/shop/?distance=${distance}&long=${lng}&lat=${lat}`;
-  try {
-    const response = await fetch(url);
-    return response.json();
-  } catch (error) {
-    return error;
-  }
+export const getNearShops = async (distance: number, lat: number, lng: number, activity?: string, active?: boolean) => {
+	const token = await AsyncStorage.getItem("token");
+
+	const url = process.env.API_URL + `/shop/?distance=${distance}&long=${lng}&lat=${lat}`;
+	if (activity) {
+		url.concat(`&activity=${activity}`);
+	}
+	if (active) {
+		url.concat(`&isActive=${active}`);
+	}
+
+	try {
+		const response = await fetch(url, {
+			method: "GET",
+			mode: "no-cors",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+				Authorization: `Bearer ${token}`
+			}
+		});
+		return response.json();
+	} catch (error) {
+		return error;
+	}
 };
