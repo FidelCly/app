078284c0e3c2db6f9907fcc192297ca83bc188b7
Andi MANIETA <andimanieta@gmail.com>
2023-08-04 11:38:09 +0200

    fix:misc

--- src/store/reducers/card.reducer.ts
+++ src/store/reducers/card.reducer.ts
@@ -4,38 +4,35 @@ import { CardActionTypes } from "../actions/card.actions";
 import { ICard, ICardState } from "../interfaces/card.interface";
 
 const initialCardState: ICardState = {
-  cards: [],
-  currentCard: null,
-  cardLoader: false,
-  cardError: null,
+	cards: [],
+	currentCard: null,
+	cardLoader: false,
+	cardError: null
 };
 
-export const getCards = createAsyncThunk(
-  CardActionTypes.GetCards,
-  async (payload: any) => {
-    const data = await getUserCards(payload);
+export const getCards = createAsyncThunk(CardActionTypes.GetCards, async (payload: any) => {
+	const data = await getUserCards();
 
-    return data;
-  }
-);
+	return data;
+});
 
 export const cardReducer = createSlice({
-  name: "cards",
-  initialState: { ...initialCardState },
-  reducers: {},
-  extraReducers(builder) {
-    builder
-      .addCase(getCards.pending, (state) => {
-        state.cardLoader = true;
-      })
-      .addCase(getCards.fulfilled, (state, action) => {
-        state.cards = action.payload as ICard[];
-        state.cardLoader = false;
-      })
-      .addCase(getCards.rejected, (state) => {
-        state.cardLoader = false;
-      });
-  },
+	name: "cards",
+	initialState: { ...initialCardState },
+	reducers: {},
+	extraReducers(builder) {
+		builder
+			.addCase(getCards.pending, (state) => {
+				state.cardLoader = true;
+			})
+			.addCase(getCards.fulfilled, (state, action) => {
+				state.cards = action.payload as ICard[];
+				state.cardLoader = false;
+			})
+			.addCase(getCards.rejected, (state) => {
+				state.cardLoader = false;
+			});
+	}
 });
 
 export default cardReducer.reducer;
